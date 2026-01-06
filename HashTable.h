27b44f6f000161d707c374660918e12498131bac
LPIQMEL <lpiqmel@alumno.upv.es@ldsic-vdi15.upvnet.upv.es>
2026-01-06 19:23:30 +0100

    Actualizado Makefile y añadido código test de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -9,7 +9,7 @@
 #include "../../P1/PRA_2526_P1/ListLinked.h"
 
 template <typename V>
-class HashTable: public Dict<V> {
+class HashTable : public Dict<V> {
 
     private:
         int n;
@@ -36,7 +36,7 @@ class HashTable: public Dict<V> {
 		delete[] table;
 	}
 
-	int capacity() {
+	int capacity() const {
 		return max;
 	}
 
@@ -56,12 +56,49 @@ class HashTable: public Dict<V> {
 
 	V operator[](std::string key){
 		for (int i = 0; i < n; i++) {
-			int pos = table[i].search(key);
+			int pos = table[i].search(TableEntry<V>(key));
 			if (pos != -1){
 				return table[i][pos].value;
 			}
 		}
 		throw std::runtime_error("Key '" + key + "' not found");
+	}
+
+	void insert(std::string key, V value) override {
+		int pos = h(key);
+		if (table[pos].search(key) != -1){
+			throw std::runtime_error("Key '" + key + "' already exists");
+		}
+		table[pos].prepend(TableEntry<V>(key, value));
+		n++;
+	}
+
+	V search(std::string key) override {
+		int idx = h(key);
+		int pos = table[idx].search(TableEntry<V>(key));
+
+		if (pos == -1){
+      			throw std::runtime_error("Key '" + key + "' not found");
+		}
+
+		return table[idx].get(pos).value;
+	}
+
+	V remove(std::string key) override {
+		int idx = h(key);
+		int pos = table[idx].search(TableEntry<V>(key));
+
+		if (pos == -1){
+			throw std::runtime_error("Key '" + key + "' not found");
+		}
+
+		n--;
+
+		return table[idx].remove(pos).value;
+	}
+
+	int entries() const override {
+		return n;
 	}
         
 };
