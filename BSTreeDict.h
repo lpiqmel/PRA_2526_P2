b030a51392c9b5d6f39e430101d69174bef91d45
LPIQMEL <lpiqmel@alumno.upv.es@ldsic-vdi15.upvnet.upv.es>
2026-01-06 22:22:05 +0100

    Añadida implementación de la clase BSTreeDict

---
+++ BSTreeDict.h
@@ -0,0 +1,53 @@
+#ifndef BSTREEDICT_H
+#define BSTREEDICT_H
+
+#include <ostream>
+#include <stdexcept>
+#include "Dict.h"
+#include "BSTree.h"
+#include "TableEntry.h"
+
+template <typename V>
+class BSTreeDict: public Dict<V> {
+
+    private:
+        BSTree<TableEntry<V>>* tree;
+
+    public:
+        BSTreeDict() {
+		tree = new BSTree<TableEntry<V>>*();
+	}
+
+	~BSTreeDict() {
+		delete tree;
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const BSTreeDict<V> &bs) {
+		out << *(bs.tree);
+		return out;
+	}
+
+	V operator[](std::string key) {
+		return search(key);
+	}
+
+       	int entries() override {
+		return tree->size();
+	}
+
+	void insert(std::string key, V value) override {
+        	tree->insert(TableEntry<V>(key, value));
+	}
+
+	V search(std::string key) override {
+		return (*this)[key];
+	}
+
+	V remove(std::string key) override {
+		V ret = search(key);
+    		tree->remove(TableEntry<V>(key));
+    		return ret;
+	}
+};
+
+#endif
