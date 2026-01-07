69a5694ba37bd78dea9085cf50e8756c6c33a5b3
LPIQMEL <lpiqmel@alumno.upv.es@ldsic-vdi15.upvnet.upv.es>
2026-01-07 19:46:28 +0100

    Cambios realizados

--- BSNode.h
+++ BSNode.h
@@ -10,7 +10,7 @@ class BSNode {
 		BSNode<T>* left;
 		BSNode<T>* right;
 
-		BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T> right = nullptr) : elem(elem), left(left), right(right) {}
+		BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr) : elem(elem), left(left), right(right) {}
 		friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn){
 			return out << bsn.elem;
 		}
